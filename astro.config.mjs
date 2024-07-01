f34d183958a8898212c8b94619d34eafb01e100a
Matthew <matthewajustice@gmail.com>
2024-07-01 18:28:58 -0400

    landing and header mostly finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.bandcpestcontrol.com/',
-  integrations: [tailwind(), preact({ compat: true })],
+  integrations: [
+    tailwind(),
+    preact({
+      compat: true,
+    }),
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
 });
